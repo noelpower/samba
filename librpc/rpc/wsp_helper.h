f1195ece1e00ae5b484ea171fa3cce732fb84201
Noel Power <noel.power@suse.com>
2016-07-26 12:23:38 +0100

    librpc/rpc: Add windows propertyset info and associated accessor and helper api.
    Signed-off-by: Noel Power <noel.power@suse.com>

--- librpc/rpc/wsp_helper.h
+++ librpc/rpc/wsp_helper.h
@@ -22,10 +22,29 @@
 #define __LIBRPC_WSP_HELPER_H__
 
 struct safearraybound;
+struct wsp_cfullpropspec;
+struct wsp_cpmgetrowsin;
+struct wsp_cpmgetrowsout;
+struct wsp_cpmsetbindingsin;
 struct wsp_hyper;
 
 uint32_t calc_array_size(struct safearraybound *bounds, uint32_t ndims);
+
+struct full_propset_info {
+	uint32_t id;
+	const char *name;
+	uint16_t vtype;
+	bool extra_info;
+	bool in_inverted_index;
+	bool is_column;
+	bool can_col_be_indexed;
+	uint16_t max_size;
+};
+char *prop_from_fullprop(TALLOC_CTX *ctx, struct wsp_cfullpropspec *fullprop);
+const struct full_propset_info *get_prop_info(const char *prop_name);
+const char * get_vtype_name(uint32_t type);
+bool is_variable_size(uint16_t vtype);
 void uint64_to_wsp_hyper(uint64_t src, struct wsp_hyper *dest);
 void wsp_hyper_to_uint64(struct wsp_hyper *src, uint64_t *dest);
-
+const char *get_store_status(uint8_t status_byte);
 #endif // __LIBRPC_WSP_HELPER_H__
